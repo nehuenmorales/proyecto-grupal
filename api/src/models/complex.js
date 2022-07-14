fd77c67eb35e8db727e8a7daaea2cbe4fd4510eb
Franciscofuhr <franciscofuhr00@gmail.com>
2022-07-14 19:16:01 -0300

    agregando id como primary key en modelos complex y tournament

--- api/src/models/complex.js
+++ api/src/models/complex.js
@@ -1,6 +1,11 @@
 const { DataTypes } = require("sequelize");
 module.exports = (sequelize) => {
   sequelize.define("complex", {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
