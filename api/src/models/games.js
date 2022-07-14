92292203f51e40179f99abe0ec8f49522f206acf
= <=>
2022-07-14 18:58:34 -0300

    model games

---
+++ api/src/models/games.js
@@ -0,0 +1,31 @@
+const { DataTypes } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define('games', {
+    date: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+    sport: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    status: {
+        type: DataTypes.ENUM('In Progress', 'Incomplete', 'Complete'),
+        allowNull: true
+    },
+    result: {
+        type: DataTypess.STRING,
+    },
+    duration: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    link: {
+        type: DataTypes.STRING
+    }
+
+  });
+};
