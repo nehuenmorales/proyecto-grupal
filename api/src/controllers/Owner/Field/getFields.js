249bfb117914b9acc47812193c3a4fc3867ca235
IvanRomano <ivan.ac.romano@gmail.com>
2022-07-20 15:02:45 -0300

    detailFields

--- api/src/controllers/Owner/Field/getFields.js
+++ api/src/controllers/Owner/Field/getFields.js
@@ -1,15 +1,28 @@
-const {Field} = require('../../../db');
+const {Field} = require("../../../db")
+const {Op} = require('sequelize')
 
-async function getFields(req,res) {
-    try {
-        const fields = await Field.findAll();
-    
-    return res.send(fields);
-    
-    } catch (error) {
-        res.send(error)
-    }
-    
+async function getFieldByName (name) {
+    let fields = await Field.findAll({
+        where: {
+                name:{
+                    [Op.iLike]:`%${name}%`
+                }
+        },
+    })
+    return fields
 }
 
-module.exports = {getFields};
+const getFields = async (req, res) =>{
+    const {name} = req.query
+    try {
+        if(name){
+        let field = await getFieldByName(name)
+        if(field) return res.json(field)
+    }
+    let fields = await Field.findAll()
+    res.json(fields)        
+    } catch (error) {
+     res.status(404).send("No se pudieron obtener las canchas")    
+    }}
+
+module.exports = {getFields}
