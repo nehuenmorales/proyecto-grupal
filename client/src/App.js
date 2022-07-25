48fd69d4a2efc3a11385afe19b3d1e65e1905768
Franciscofuhr <franciscofuhr00@gmail.com>
2022-07-24 21:44:08 -0300

    terminado el form de supplies

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,10 @@ import React from "react";
 import { Route } from "react-router-dom";
 import CreateFields from "./components/Fields/CreateFields/CreateFields";
 import CreateSupplies from "./components/Supplies/CreateSupplies/createSupplies";
+import SuppliesFutbol from "./components/Supplies/CreateSupplies/Futbol/suppliesFutbol";
+import SuppliesBasquet from "./components/Supplies/CreateSupplies/Basquet/suppliesBasquet";
+import SuppliesPadel from "./components/Supplies/CreateSupplies/Padel/suppliesPadel";
+import SuppliesTenis from "./components/Supplies/CreateSupplies/Tenis/suppliesTenis";
 //import Home from './Home.component';
 
 function App() {
@@ -12,6 +16,12 @@ function App() {
 
 
       <Route exact path={"/owner/createSupplie"} component={CreateSupplies}/>
+      <Route exact path={"/owner/createSupplie/futbol"} component={SuppliesFutbol}/>
+      <Route exact path={"/owner/createSupplie/tenis"} component={SuppliesTenis}/>
+      <Route exact path={"/owner/createSupplie/padel"} component={SuppliesPadel}/>
+      <Route exact path={"/owner/createSupplie/basquet"} component={SuppliesBasquet}/>
+
+
     </>
   ) 
 }
