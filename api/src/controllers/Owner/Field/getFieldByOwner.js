16b49cb9a59dfc091b9623610bc980593a79fad7
LaraCorvalan <laricorvalan94@gmail.com>
2022-07-29 19:16:48 -0300

    cambio ruta getFieldByOwner

--- api/src/controllers/Owner/Field/getFieldByOwner.js
+++ api/src/controllers/Owner/Field/getFieldByOwner.js
@@ -6,10 +6,21 @@ async function getFieldByOwner (req, res){
         const getComplex = await Complex.findAll({
           where:{ownerId : id}
         });
-        const getField = await Field.findAll({
-            where: {complexId : getComplex.id}
-        })
-        res.status(200).json(getField);
+        const result = []
+        const filtrados = [];
+        for (let i = 0; i < getComplex.length; i++) {
+           filtrados.push(getComplex[i].id)
+        }
+        for (let j = 0; j < filtrados.length; j++) {
+             result = [...result, await Field.findAll({
+                where: {complexId : filtrados[j]}
+             })]
+        }
+        console.log('soy result', result)
+        // const getField = await Field.findAll({
+        //     where: {complexId : filtrados}
+        // })
+        res.status(200).json(result);
       } catch (e) {
         console.log(e);
         res.status(400).json({ msg: "no hay reservas" });
