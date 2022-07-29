ee035e1f98b9e9e834b755ba32e346d2636fec8b
LaraCorvalan <laricorvalan94@gmail.com>
2022-07-28 22:10:06 -0300

    getOwner controller

--- api/src/controllers/Owner/owner/getOwner.js
+++ api/src/controllers/Owner/owner/getOwner.js
@@ -3,14 +3,14 @@ const { Owner } = require("../../../db");
 async function getOwner(req, res, next) {
     // const {email} = req.params
     // console.log(email, 'soy id')
-  try {
-    console.log('entro al controller')
-   const owner = await Owner.findAll()
-      res.json(owner)
-  } catch (err) {
-    console.log(err, 'error')
-    res.status(400).json({msg:'no se encontro nada'});
-  }
+    try {
+      const allPlayers = await Owner.findAll()
+  
+      res.send(allPlayers)
+  
+    } catch (error) {
+      res.status(400).json(error);
+    }
 }
 
 module.exports = {
