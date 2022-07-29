92f08dd7dbf308bb886c73fdeb6c71d027177b1d
LaraCorvalan <laricorvalan94@gmail.com>
2022-07-29 19:35:47 -0300

    a ver si funciona

--- api/src/controllers/Owner/Field/getFieldByOwner.js
+++ api/src/controllers/Owner/Field/getFieldByOwner.js
@@ -4,19 +4,19 @@ async function getFieldByOwner (req, res){
     const {id} = req.params
       try {
         const getComplex = await Complex.findAll({
-          where:{ownerId : id}
+          where:{ownerId : id},
+          include : [{model:Field}]
         });
-        let result = []
-        let filtrados = [];
-        for (let i = 0; i < getComplex.length; i++) {
-           filtrados.push(getComplex[i].id)
-        }
-        
-        console.log('soy result', result)
+
+        // let filtrados = [];
+        // for (let i = 0; i < getComplex.length; i++) {
+        //    filtrados.push(getComplex[i].id)
+        // }
+       
         // const getField = await Field.findAll({
         //     where: {complexId : filtrados}
         // })
-        res.status(200).json(filtrados);
+        res.status(200).json(getComplex);
       } catch (e) {
         console.log(e);
         res.status(400).json({ msg: "no hay reservas" });
