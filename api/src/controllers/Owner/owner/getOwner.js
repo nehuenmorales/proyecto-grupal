0b6f1243d6012aa723cf4355781804ce156bb83f
LaraCorvalan <laricorvalan94@gmail.com>
2022-07-28 22:02:42 -0300

    ?

--- api/src/controllers/Owner/owner/getOwner.js
+++ api/src/controllers/Owner/owner/getOwner.js
@@ -5,13 +5,8 @@ async function getOwner(req, res, next) {
     // console.log(email, 'soy id')
   try {
     console.log('entro al controller')
-   const owner = await Owner.findAll(
-    // {
-    //    where:{
-    //        email: email
-    //    }}
-      )
-      res.send(owner)
+   const owner = await Owner.findAll()
+      res.json(owner)
   } catch (err) {
     console.log(err, 'error')
     res.status(400).json({msg:'no se encontro nada'});
