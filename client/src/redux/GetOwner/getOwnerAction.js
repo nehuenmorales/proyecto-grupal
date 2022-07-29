9882eb43cc87c97fbe3a2cf94dec01a86aec5f7f
MaguiHeredia <maguiherediavaiarini@gmail.com>
2022-07-28 23:03:44 -0300

    develop funcionando

--- client/src/redux/GetOwner/getOwnerAction.js
+++ client/src/redux/GetOwner/getOwnerAction.js
@@ -8,6 +8,7 @@ export const getOwner =(id)=>{
         console.log(id, 'entre a las action')
         return axios.get(`https://falta-uno-1.herokuapp.com/owner/getOwner/${id}`)
         .then((res)=> {
+            console.log('get Owner', res.data)
             return dispatch({type:GET_OWNER,payload:res.data})})
     }   
 }
