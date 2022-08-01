c753ba56f4775bcda04acd5ea2fd959134760228
Franciscofuhr <franciscofuhr00@gmail.com>
2022-08-01 11:36:13 -0300

    hola

--- api/src/models/teams.js
+++ api/src/models/teams.js
@@ -9,7 +9,7 @@ module.exports = (sequelize) => {
     "teams",
     {
       id:{
-        type:DataTypes.UUIDV4,
+        type:DataTypes.UUID,
         allowNull: false,
         defaultValue: UUIDV4,
         primaryKey: true
