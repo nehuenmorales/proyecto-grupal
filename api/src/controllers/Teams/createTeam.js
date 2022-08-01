516c318804a9e1de827ee0d167e34c8fa05d3fc9
Franciscofuhr <franciscofuhr00@gmail.com>
2022-08-01 03:50:30 -0300

    plischus

--- api/src/controllers/Teams/createTeam.js
+++ api/src/controllers/Teams/createTeam.js
@@ -1,22 +1,63 @@
-const { Teams, Player, conn } = require("../../db.js");
-const { Op } = require("sequelize");
-const { QueryTypes } = require("sequelize");
+// const { Teams, Player, conn } = require("../../db.js");
+// const { Op } = require("sequelize");
+// const { QueryTypes } = require("sequelize");
 
+
+// async function createTeam(req, res, next) {
+//   const { name, image, sport, playerEmail, amountPlayers } = req.body;
+//   try {
+//     // const newTeam = await conn.query(
+//     //   `(INSERT INTO teams(name,  image, sport, "amountPlayers")
+//     //    VALUES (:name,:image, :sport, :amountplayers))`,
+//     //   {
+//     //     replacements: { name:name,
+//     //      image:image,
+//     //     sport:sport,
+//     //   amountplayers:amountPlayers},
+//     //     type: QueryTypes.SELECT,
+//     //   }
+//     // );
+//     const player = await Player.findOne({
+//       where: {
+//         email: {
+//           [Op.eq]: playerEmail,
+//         },
+//       },
+//     });
+//     // await newTeam.addPlayer(player);
+
+//     //FALTA LA RUTA DE CREACION DEL USUARIO/COMPLEJO para linkear a un complejo
+
+//     // busco el complejo que crea la cancha
+//     // const complexField = await Complex.findOne({
+//     //   where: { id: complexId },
+//     // });
+//     // newField.addField(complexField); // asocio la cancha con el complejo
+//     res.status(200).json(newTeam);
+//   } catch (e) {
+//     console.log("fallo la creacion de la cancha", e);
+//     res.status(400).json(e);
+//   }
+// }
+
+// module.exports = {
+//   createTeam,
+// };
+
+
+
+const { Teams, Player } = require("../../db.js");
+const { Op } = require("sequelize");
 
 async function createTeam(req, res, next) {
   const { name, image, sport, playerEmail, amountPlayers } = req.body;
   try {
-    // const newTeam = await conn.query(
-    //   `(INSERT INTO teams(name,  image, sport, "amountPlayers")
-    //    VALUES (:name,:image, :sport, :amountplayers))`,
-    //   {
-    //     replacements: { name:name,
-    //      image:image,
-    //     sport:sport,
-    //   amountplayers:amountPlayers},
-    //     type: QueryTypes.SELECT,
-    //   }
-    // );
+    const newTeam = await Teams.create({
+      name,
+      image,
+      sport,
+      amountPlayers,
+    });
     const player = await Player.findOne({
       where: {
         email: {
@@ -24,7 +65,7 @@ async function createTeam(req, res, next) {
         },
       },
     });
-    // await newTeam.addPlayer(player);
+    await newTeam.addPlayer(player);
 
     //FALTA LA RUTA DE CREACION DEL USUARIO/COMPLEJO para linkear a un complejo
 
@@ -36,7 +77,7 @@ async function createTeam(req, res, next) {
     res.status(200).json(newTeam);
   } catch (e) {
     console.log("fallo la creacion de la cancha", e);
-    res.status(400).json(e);
+    res.status(400).json({ msg: "fallo la creacion de la cancha" });
   }
 }
 
