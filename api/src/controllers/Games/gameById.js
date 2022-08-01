317b8e9463b7285d819022ad206f67cde337a418
MaguiHeredia <maguiherediavaiarini@gmail.com>
2022-07-31 23:44:56 -0300

    otra vez address

--- api/src/controllers/Games/gameById.js
+++ api/src/controllers/Games/gameById.js
@@ -7,7 +7,7 @@ const { Games } = require ("../../db.js")
 async function detailGame (req, res, next){
     const {id} = req.params
     try{
-    const detail= await conn.query(`(SELECT g.*, f.name,f.image, f."pricePerTurn", x.adress, x.name AS complexName, x.city, f.capacity, f.start AS open, f.end AS close
+    const detail= await conn.query(`(SELECT g.*, f.name,f.image, f."pricePerTurn", x.address, x.name AS complexName, x.city, f.capacity, f.start AS open, f.end AS close
         FROM "games" g
         JOIN fields f ON g."fieldId" = f.id
         JOIN complexes x ON x.id=f."complexId"
