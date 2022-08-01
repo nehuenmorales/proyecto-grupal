165b7b500b0682799ad5aded7614924abcb12a4b
unknown <ailumacchia@hotmail.com>
2022-08-01 20:51:35 -0300

    prueba

--- client/src/redux/OwnerComplex/complexActions.js
+++ client/src/redux/OwnerComplex/complexActions.js
@@ -5,7 +5,7 @@ export const CREATE_COMPLEX = "CREATE_COMPLEX";
 export const createComplex =(body)=>{
     console.log("llega a las actions create complex, soy id", body)
     return async function (dispatch){
-        return axios.post("https://falta-uno-1.herokuapp.com/owner/createComplex",body)
+        return axios.post("/owner/createComplex",body)
         .then((res)=> {
             console.log("respuesta del post",res.data)
             return dispatch({type:CREATE_COMPLEX,payload:res.data})})    
