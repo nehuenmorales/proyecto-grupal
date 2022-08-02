16281cef7af78e203cf554e6c62281a0bb6839d1
unknown <ailumacchia@hotmail.com>
2022-08-02 00:26:05 -0300

    funciona!

--- api/src/models/games.js
+++ api/src/models/games.js
@@ -1,5 +1,4 @@
 const { DataTypes } = require("sequelize");
-const { UUIDV4 } = require('sequelize');
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
