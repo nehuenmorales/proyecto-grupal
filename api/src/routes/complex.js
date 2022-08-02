aa5f63583ab9977e72d1d35a3754a2b6090b53e8
unknown <ailumacchia@hotmail.com>
2022-08-02 20:03:00 -0300

    porfa

--- api/src/routes/complex.js
+++ api/src/routes/complex.js
@@ -1,14 +1,12 @@
 const {Router} = require('express');
 const { getComplex } = require("../controllers/Complex/getComplex");
 
-const { createComplex } = require('../controllers/Owner/complex/createComplex');
 const {getSearchComplex} = require('../controllers/Complex/getSearchComplex');
 const { getCities } = require('../controllers/apiCities/getCities');
 
 const router = Router();
 
 router.get('/all', getComplex)
-router.post('/createComplex', createComplex)
 router.get('/:sport/searchComplex', getSearchComplex)
 router.get('/getCities', getCities)
 
