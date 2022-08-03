3f883c4a9f1e73640a608bb635ce9e3dde43169a
LaraCorvalan <laricorvalan94@gmail.com>
2022-08-03 20:45:29 -0300

    renderizo datos en fieldDetail

--- client/src/components/DetailOwner/Fields/FieldDetail.jsx
+++ client/src/components/DetailOwner/Fields/FieldDetail.jsx
@@ -16,13 +16,68 @@ export default function FieldDetail({ id }) {
     return (
         <div>
             <div>
-                <Link to='/' style={{ 'padding': '10px', 'width': '25%', 'margin': '20px 10px 10px 10px' }}>
+                <Link to='/fieldOwner' style={{ 'padding': '10px', 'width': '25%', 'margin': '20px 10px 10px 10px' }}>
                     <Button style={{ 'marginTop': '15px' }}>Volver</Button>
                 </Link>
             </div>
+            <div className="tituloName">
+                <h2 className="fw-normal text-white fst-italic m-9" style={{ fontSize: '3em', marginLeft: '35%' }}>{field.name}</h2>
+                <h5>{field.complexId}</h5>
+            </div>
             <div>
-                <h1>Detail field</h1>
+                <form /*onSubmit={(e) => handleSubmit(e)} */>
+                    <div className='contenedorLapiz'>
+                        <p className="subTitulos">Descripción</p>
+                        <img src='https://cdn-icons-png.flaticon.com/512/1250/1250615.png' className='lapiz'></img>
+                    </div>
+                    <textarea className="infoForm" name='description' /*onChange={ev => onClick(ev)} */ value={field.description} />
+                    {/* {errors.description ? <div className="errores" style={{ marginLeft: '80px' }}>{errors.description}</div> : null} */}
+                    <div className='contenedorLapiz'>
+                        <p className="subTitulos">Capacidad de jugadores</p>
+                        <img src='https://cdn-icons-png.flaticon.com/512/1250/1250615.png' className='lapiz'></img>
+                    </div>
+                    <input className="infoForm" name='description' /*onChange={ev => onClick(ev)} */ value={field.capacity} />
+
+                    <div className='contenedorLapiz'>
+                        <p className="subTitulos">Precio por turno</p>
+                        <img src='https://cdn-icons-png.flaticon.com/512/1250/1250615.png' className='lapiz'></img>
+                    </div>
+                    <input className="infoForm" name='description' /*onChange={ev => onClick(ev)} */ value={field.pricePerTurn} />
+
+                    <div className='contenedorLapiz'>
+                        <p className="subTitulos">Horario de apertura de la cancha</p>
+                        <img src='https://cdn-icons-png.flaticon.com/512/1250/1250615.png' className='lapiz'></img>
+                    </div>
+                    <input className="infoForm" name='description' /*onChange={ev => onClick(ev)} */ value={field.start} />
+                    
+                    <div className='contenedorLapiz'>
+                        <p className="subTitulos">Horario de cierre de la cancha</p>
+                        <img src='https://cdn-icons-png.flaticon.com/512/1250/1250615.png' className='lapiz'></img>
+                    </div>
+                    <input className="infoForm" name='description' /*onChange={ev => onClick(ev)} */ value={field.end} />
+                    
+                    <div className='contenedorLapiz'>
+                        <p className="subTitulos">Duración por turno</p>
+                        <img src='https://cdn-icons-png.flaticon.com/512/1250/1250615.png' className='lapiz'></img>
+                    </div>
+                    <input className="infoForm" name='description' /*onChange={ev => onClick(ev)} */ value={field.durationPerTurn} />
+
+                    <div className='contenedorLapiz'>
+                        <p className="subTitulos">Deporte</p>
+                        <img src='https://cdn-icons-png.flaticon.com/512/1250/1250615.png' className='lapiz'></img>
+                    </div>
+                    <input className="infoForm" name='description' /*onChange={ev => onClick(ev)} */ value={field.sport} />
+
+
+                </form>
+            </div>
+            <div style={{ backgroundImage: `url(${field.image})` }} className='derecha'>
+                <div className='div-rating' style={{ backgroundColor: `rgba(17, 24, 37, 1)`, padding: '7px 15px 3px', height: '50px', width: '150px' }}>
+                    <img src="https://api.iconify.design/material-symbols:star-rounded.svg?color=%23ffee00" alt="" className="rating-img" style={{ height: `28px` }} />
+                    <p className="complex-rating" style={{ fontSize: `1.3em`, textShadow: '1px 1px 3px black;' }}> {field.available}</p>
+                </div>
             </div>
         </div>
+
     )
 }
