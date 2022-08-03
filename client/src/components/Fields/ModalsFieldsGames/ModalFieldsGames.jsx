a83ca8c3a95f8a4fd2555032dfd50d9f99ecc882
MaguiHeredia <maguiherediavaiarini@gmail.com>
2022-08-02 21:13:04 -0300

    todo ok

--- client/src/components/Fields/ModalsFieldsGames/ModalFieldsGames.jsx
+++ client/src/components/Fields/ModalsFieldsGames/ModalFieldsGames.jsx
@@ -201,7 +201,7 @@ export default function ModalsFieldsGames({ showModal, setShowModal, setNewField
             <Modal show={showModal} size="lg" aria-labelledby="example-modal-sizes-title-lg">
                 <Modal.Header className={s.contenedorTitle}>
                     <Modal.Title>
-                            <h2 className={s.titulo}>Esquema de turnos holisss</h2>
+                            <h2 className={s.titulo}>Esquema de turnos</h2>
                             <p className={s.subTitulo}>Selecciona en rojo los turnos que NO se deben crear</p>
                     </Modal.Title>
                 </Modal.Header>
