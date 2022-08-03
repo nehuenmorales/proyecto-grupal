c25204f17d435b75e5ca2c98a11556c7cd4c73ff
leogonzalezok <leogonzalez777.lg@gmail.com>
2022-08-03 18:51:22 -0300

    Para el elias que queria un componentito fachero

--- client/src/components/MercadoPago/success.jsx
+++ client/src/components/MercadoPago/success.jsx
@@ -1,41 +1,50 @@
-import React,{useEffect} from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux'
 import { useAuth0 } from '@auth0/auth0-react';
 import { updateGame } from '../../redux/Games/gamesAction';
 import { putGame } from '../../redux/GamesIncomplete/gamesIncompleteActions';
+import { Container, Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import VerticalNavbar from '../VerticalNavbar/VerticalNavbar'
 
+const Success = ({ match }) => {
 
+  const dispatch = useDispatch()
+  const id = match.params.id;
+  const { user, isLoading } = useAuth0();
+  const valores = window.location.search;
+  const urlParams = new URLSearchParams(valores);
+  let status = urlParams.get('status');
 
-const Success = ({match}) => {
-    
-    const dispatch = useDispatch()
-    const id = match.params.id;
-    const {user} = useAuth0();
-    const valores = window.location.search
-    console.log(valores)
-    const urlParams = new URLSearchParams(valores);
-    let status = urlParams.get('status');
-    console.log(status)
-    console.log(id,"id")
-    console.log(user.email)
+  useEffect(() => {
 
-    useEffect(() => {
-        if(status==="approved"){
-            dispatch(updateGame(id, {status:"booked"}))
-            dispatch(putGame(id, { email: user.email }));
-        }else{
-            dispatch(updateGame(id, {status:"free"})) 
-        }
-        
-        console.log("entro")
-    }, [dispatch]);
+    if (status === "approved") {
+      dispatch(updateGame(id, { status: "booked" }))
+      if (!isLoading) {
+        dispatch(putGame(id, { email: user.email }));
+      }
+    } else {
+      dispatch(updateGame(id, { status: "free" }))
+    }
+  }, [dispatch, isLoading]);
 
 
   return (
-    <div>
-    {/* <VerticalNavbar/> */}
-    <h1>Tu Cancha fue reservada con exito!</h1>
-    </div>
+    <Container>
+      <VerticalNavbar />
+      {
+        isLoading ?
+          <p>Cargando....</p>
+          :
+          status === 'approved' 
+          ? null
+          :null
+      }
+      <h1 className='text-white'>Tu Cancha fue reservada con exito! :)</h1>
+      <Link to='/'>
+        <Button >Volver al Inicio</Button>
+      </Link>
+    </Container>
   )
 }
 
