1e1633ee9d6b8d104d43feeeb55102d48d7fc172
MaguiHeredia <maguiherediavaiarini@gmail.com>
2022-08-04 20:42:28 -0300

    empezando con el front booked game

--- client/src/components/DetailOwner/BookedGames/BookedGames.jsx
+++ client/src/components/DetailOwner/BookedGames/BookedGames.jsx
@@ -1,21 +1,48 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useSelector } from 'react-redux';
 
 
-export default function BookedGames(){
+export default function BookedGames() {
     let owner = useSelector((state) => state.getOwnerReducer.owner)
+    const [bookedGames, setBookedGames] = useState([])
+    const [bookedGame, setBookedGame] = useState({
+        date: "",
+        end: "",
+        fieldId: '',
+        id: '',
+        link: '',
+        privacy: "",
+        requirements: '',
+        result: '',
+        sport: "",
+        start: "",
+        status: "",
+        tournamentId: '',
+        type: ""
+    })
 
-    useEffect(()=> {
+    useEffect(() => {
         axios.get(`https://falta-uno-1.herokuapp.com/owner/getBookedGamesByOwner/${owner.id}`)
-        .then(res => console.log('soy respuestaa', res.data));
+            .then(res => {
+                for (let i = 0; i < res.data.length; i++) {
+                    for (let j = 0; j < res.data[i].length; j++) {
+                        console.log('soy res i j',res.data[i][j])
+                        if(res.data[i][j].status === 'booked'){
+                            setBookedGames([...bookedGames, res.data[i][j]])
+                        }
+                    }
+                    
+                }
+                console.log('soy respuestaa', res.data)
+            });
     }, [])
+    console.log('soy booked game', bookedGames)
+    console.log('booked games owner', owner)
 
-     console.log('booked games owner', owner)
-
-    return(
+    return (
         <div>
-            <h1 style={{color: 'white'}}>Booked Games</h1>
-        </div>  
+            <h1 style={{ color: 'white' }}>Booked Games</h1>
+        </div>
     )
 }
