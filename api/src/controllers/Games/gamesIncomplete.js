c6ea2e6641636b04bb6bcadd8a15f318cf0f0050
= <=>
2022-08-03 23:38:48 -0300

    asdfsafd

--- api/src/controllers/Games/gamesIncomplete.js
+++ api/src/controllers/Games/gamesIncomplete.js
@@ -6,10 +6,10 @@ async function gamesIncomplete(req,res,next){
     try{
         const player= await conn.query(`(SELECT count_player_by_game.*, f.capacity - enrolled_amount AS freePlace, f.*
             FROM (
-                SELECT g.id as gameid, g.start AS startHour,g.end AS endHour,g."fieldId" ,count(*) AS enrolled_amount
+                SELECT g.id as gameid, g.privacy, g.start AS startHour,g.end AS endHour,g."fieldId" ,count(*) AS enrolled_amount
                 FROM games g
                 JOIN player_games pbg ON pbg."gameId" = g.id
-                
+                WHERE g.privacy = 'public'
                 GROUP BY g.id
             ) count_player_by_game
             JOIN fields f ON count_player_by_game."fieldId" = f.id
