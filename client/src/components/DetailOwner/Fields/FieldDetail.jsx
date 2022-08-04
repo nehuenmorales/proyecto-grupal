edb40616237f6f2e5a16aa46061bf5f7e04d39ce
LaraCorvalan <laricorvalan94@gmail.com>
2022-08-03 21:14:42 -0300

    actualizo fieldDetail y agrego css

--- client/src/components/DetailOwner/Fields/FieldDetail.jsx
+++ client/src/components/DetailOwner/Fields/FieldDetail.jsx
@@ -14,17 +14,17 @@ export default function FieldDetail({ id }) {
     }, [])
 
     return (
-        <div>
-            <div>
+        <div className="contenedorDetail">
+            <div className='izquierda'>
                 <Link to='/fieldOwner' style={{ 'padding': '10px', 'width': '25%', 'margin': '20px 10px 10px 10px' }}>
                     <Button style={{ 'marginTop': '15px' }}>Volver</Button>
                 </Link>
-            </div>
+            
             <div className="tituloName">
                 <h2 className="fw-normal text-white fst-italic m-9" style={{ fontSize: '3em', marginLeft: '35%' }}>{field.name}</h2>
                 <h5>{field.complexId}</h5>
             </div>
-            <div>
+            
                 <form /*onSubmit={(e) => handleSubmit(e)} */>
                     <div className='contenedorLapiz'>
                         <p className="subTitulos">Descripción</p>
@@ -33,7 +33,7 @@ export default function FieldDetail({ id }) {
                     <textarea className="infoForm" name='description' /*onChange={ev => onClick(ev)} */ value={field.description} />
                     {/* {errors.description ? <div className="errores" style={{ marginLeft: '80px' }}>{errors.description}</div> : null} */}
                     <div className='contenedorLapiz'>
-                        <p className="subTitulos">Capacidad de jugadores</p>
+                        <p className="subTitulos">Capacidad total de jugadores</p>
                         <img src='https://cdn-icons-png.flaticon.com/512/1250/1250615.png' className='lapiz'></img>
                     </div>
                     <input className="infoForm" name='description' /*onChange={ev => onClick(ev)} */ value={field.capacity} />
@@ -42,25 +42,25 @@ export default function FieldDetail({ id }) {
                         <p className="subTitulos">Precio por turno</p>
                         <img src='https://cdn-icons-png.flaticon.com/512/1250/1250615.png' className='lapiz'></img>
                     </div>
-                    <input className="infoForm" name='description' /*onChange={ev => onClick(ev)} */ value={field.pricePerTurn} />
+                    <input className="infoForm" name='description' /*onChange={ev => onClick(ev)} */ value={`$${field.pricePerTurn}`} />
 
                     <div className='contenedorLapiz'>
                         <p className="subTitulos">Horario de apertura de la cancha</p>
                         <img src='https://cdn-icons-png.flaticon.com/512/1250/1250615.png' className='lapiz'></img>
                     </div>
-                    <input className="infoForm" name='description' /*onChange={ev => onClick(ev)} */ value={field.start} />
+                    <input className="infoForm" name='description' /*onChange={ev => onClick(ev)} */ value={`${field.start} + hs`} />
                     
                     <div className='contenedorLapiz'>
                         <p className="subTitulos">Horario de cierre de la cancha</p>
                         <img src='https://cdn-icons-png.flaticon.com/512/1250/1250615.png' className='lapiz'></img>
                     </div>
-                    <input className="infoForm" name='description' /*onChange={ev => onClick(ev)} */ value={field.end} />
+                    <input className="infoForm" name='description' /*onChange={ev => onClick(ev)} */ value={`${field.end} + hs`} />
                     
                     <div className='contenedorLapiz'>
                         <p className="subTitulos">Duración por turno</p>
                         <img src='https://cdn-icons-png.flaticon.com/512/1250/1250615.png' className='lapiz'></img>
                     </div>
-                    <input className="infoForm" name='description' /*onChange={ev => onClick(ev)} */ value={field.durationPerTurn} />
+                    <input className="infoForm" name='description' /*onChange={ev => onClick(ev)} */ value={`${field.durationPerTurn} + hs`} />
 
                     <div className='contenedorLapiz'>
                         <p className="subTitulos">Deporte</p>
@@ -74,7 +74,19 @@ export default function FieldDetail({ id }) {
             <div style={{ backgroundImage: `url(${field.image})` }} className='derecha'>
                 <div className='div-rating' style={{ backgroundColor: `rgba(17, 24, 37, 1)`, padding: '7px 15px 3px', height: '50px', width: '150px' }}>
                     <img src="https://api.iconify.design/material-symbols:star-rounded.svg?color=%23ffee00" alt="" className="rating-img" style={{ height: `28px` }} />
-                    <p className="complex-rating" style={{ fontSize: `1.3em`, textShadow: '1px 1px 3px black;' }}> {field.available}</p>
+                    <div className="complex-rating" /*style={{ fontSize: `1.3em`, textShadow: '1px 1px 3px black;' }}*/> {
+                        field.available === 'true' ? 
+                        <div>
+                            <p>Disponible</p>
+                            <img src="https://cdn-icons-png.flaticon.com/512/190/190411.png" alt="" />
+                        </div>
+                         :
+                         <div>
+                            <p>Actualmente no disponible</p>
+                            <img src="https://cdn-icons.flaticon.com/png/512/1008/premium/1008927.png?token=exp=1659572730~hmac=56f58e38a6705cd818eb3ada627fc3df" alt="" />
+                         </div>
+                        
+                    }</div>
                 </div>
             </div>
         </div>
