d859526651f7d646bdac52d93d0c2e1c7f68fc10
LaraCorvalan <laricorvalan94@gmail.com>
2022-08-05 00:58:49 -0300

    tabla para ver reservas

--- client/src/components/DetailOwner/BookedGames/TableGames.jsx
+++ client/src/components/DetailOwner/BookedGames/TableGames.jsx
@@ -4,7 +4,7 @@ import Table from 'react-bootstrap/Table';
 export default function TableGames({ bookedGames, totalGames }) {
     return (
         <div>
-            {totalGames?.length > 0 ?
+            {bookedGames?.length > 0 ?
                 <Table striped>
                     <thead>
                         <tr>
@@ -15,10 +15,10 @@ export default function TableGames({ bookedGames, totalGames }) {
                         </tr>
                     </thead>
                     <tbody>
-                        {totalGames?.map((elem) => {
+                        {bookedGames?.map((elem) => {
                             return (
                                 <tr>
-                                    <td style={{color:'white'}}>{elem.field?.name}</td>
+                                    <td style={{color:'white'}}>{elem.field.name}</td>
                                     <td style={{color:'white'}}>{elem.date}</td>
                                     <td style={{color:'white'}}>{elem.start}hs</td>
                                     <td style={{color:'white'}}>{elem.end}hs</td>
