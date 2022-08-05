cd8eded9b9e3bd6f3a58e8db7d0074d37d10396c
unknown <ailumacchia@hotmail.com>
2022-08-05 17:25:42 -0300

    cambio back

--- api/src/controllers/Owner/Field/getFields.js
+++ api/src/controllers/Owner/Field/getFields.js
@@ -4,9 +4,10 @@ const { conn } = require('../../../db.js');
 async function detailFields (req, res, next){
     const {id} = req.params
     try{
-    const detail= await conn.query(`(SELECT g.*, f.name, f."pricePerTurn", f."durationPerTurn", f.capacity, f.start AS open, f.end AS close
+    const detail= await conn.query(`(SELECT g.*,x.*, f.name, f."pricePerTurn", f."durationPerTurn", f.capacity, f.start AS open, f.end AS close
         FROM "games" g
         JOIN fields f ON g."fieldId" = f.id
+        JOIN complexes x ON f."complexId" = x.id
         WHERE g.status = 'free' 
         AND f.id = :id)`,
                 {
@@ -24,8 +25,9 @@ async function getFields (req, res, next){
     const {id} = req.params
     const {sport} = req.params
     try{
-    const allGames= await conn.query(`(SELECT f.*
+    const allGames= await conn.query(`(SELECT f.*,x.*
         FROM "fields" f
+        JOIN complexes x ON f."complexId" = x.id
         WHERE f.sport = :sport)`,
                 {
                     replacements: { sport: sport},
