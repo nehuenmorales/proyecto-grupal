c5aa9ddfbd0e2495834d21495735a1b45d7a1813
MaguiHeredia <maguiherediavaiarini@gmail.com>
2022-08-08 20:49:19 -0300

    console.log

--- api/src/controllers/Owner/owner/createOwner.js
+++ api/src/controllers/Owner/owner/createOwner.js
@@ -11,7 +11,7 @@ async function createOwner(req, res, next) {
       telephone: telephone,
       username: username,
     });
-
+    console.log('playerrr',player)
     return res.json(player).status(200);
   } catch (err) {
     console.log("el error en el controllerrrrr", err);
