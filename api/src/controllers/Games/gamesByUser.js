a7059fbf594425eb575902d62f7af4410258f3fc
unknown <ailumacchia@hotmail.com>
2022-08-08 20:30:27 -0300

    help

--- api/src/controllers/Games/gamesByUser.js
+++ api/src/controllers/Games/gamesByUser.js
@@ -6,23 +6,23 @@ async function gamesByUser(req, res, next) {
     const { email } = req.params
     console.log(req.params, "es el body") 
     try {
-        const gamesUser = await Player.findOne({
-            where: {
-                email: email
-            },
-            include: [{model: Games}]
-        })
-        // const gamesUser= await conn.query(`(SELECT p.username,p.email,g.*,f.name, f.capacity, f."pricePerTurn", x.*
-        // FROM players p
-        // JOIN player_games pg ON pg."playerId" = p.id
-        // JOIN games g ON pg."gameId" = g.id
-        // JOIN fields f ON g."fieldId" = f.id
-        // JOIN complexes x ON f."complexId" = x.id
-        // WHERE p.email = :email)`,
-        //                 {
-        //                     replacements: { email: email},
-        //                     type: QueryTypes.SELECT
-        //                 })
+        // const gamesUser = await Player.findOne({
+        //     where: {
+        //         email: email
+        //     },
+        //     include: [{model: Games}]
+        // })
+        const gamesUser= await conn.query(`(SELECT p.email,g.*,f.name AS "fieldname", x.id AS "complexid"
+        FROM players p
+        JOIN player_games pg ON pg."playerId" = p.id
+        JOIN games g ON pg."gameId" = g.id
+        JOIN fields f ON g."fieldId" = f.id
+        JOIN complexes x ON f."complexId" = x.id
+        WHERE p.email = :email)`,
+                        {
+                            replacements: { email: email},
+                            type: QueryTypes.SELECT
+                        })
         res.send(gamesUser)
     }catch(e){
         res.send(e)
