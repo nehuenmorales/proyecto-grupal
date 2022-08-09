e51a4485247bafa0f22dc080b6baa005e5889779
MaguiHeredia <maguiherediavaiarini@gmail.com>
2022-08-09 09:52:14 -0300

    templete

--- client/src/components/Contact/Contact.jsx
+++ client/src/components/Contact/Contact.jsx
@@ -7,7 +7,7 @@ export default function Contact (){
     
         const enviarEmail = (e) => {
             e.preventDefault();
-            emailjs.sendForm('service_e31gpn6','template_8z2hnjl',e.target, '8pAhqJrxv2RO5fnUg').then(res=>{
+            emailjs.sendForm('service_e31gpn6','template_kcvpz8a',e.target, '8pAhqJrxv2RO5fnUg').then(res=>{
                 console.log(res);
             })
         }
