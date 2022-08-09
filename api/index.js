1a9d3503bd074ef7d3fc970132fe41be8c3a2a0f
MaguiHeredia <maguiherediavaiarini@gmail.com>
2022-08-09 12:41:34 -0300

    force true

--- api/index.js
+++ api/index.js
@@ -23,7 +23,7 @@ const app = require("./src/app.js");
 const { conn } = require("./src/db.js");
 
 // Syncing all the models at once.
-conn.sync({ force: false}).then(() => {
+conn.sync({ force: true}).then(() => {
   app.listen(process.env.PORT, () => {
     console.log(`%s listening at ${process.env.PORT}`); // eslint-disable-line no-console
   });
