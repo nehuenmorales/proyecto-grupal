a65d030809fb6493cd5a9bd0300b9e2f07edcf8d
unknown <ailumacchia@hotmail.com>
2022-08-10 22:09:27 -0300

    css rating

--- client/src/components/Games/GamesByUser/ModalRating.jsx
+++ client/src/components/Games/GamesByUser/ModalRating.jsx
@@ -1,19 +1,19 @@
 import React, { useEffect, useState } from "react";
 import Modal from 'react-bootstrap/Modal';
 import StarRating from "../../Rating/starRating";
-
+import { Heading } from '@chakra-ui/react'
 
 export default function ModalRating({ showModal, setShowModal, complex }) {
     
     return (
-            <Modal show={showModal} size="lg" aria-labelledby="example-modal-sizes-title-lg">
+            <Modal show={showModal} size="md" aria-labelledby="example-modal-sizes-title-lg">
                 <Modal.Header>
                     <Modal.Title>
-                        <h2 >Califica tu experiencia</h2>
-                        <p>Queremos saber como la pasaste en turno</p>
+                        <Heading style={{display:"flex",alignItems:"center", marginLeft:"45px"}}>Califica tu experiencia</Heading>
+                        <p style={{fontSize:"18px",marginLeft:"45px"}}>Queremos saber como la pasaste en tu turno</p>
                     </Modal.Title>
                 </Modal.Header>
-                <Modal.Body >
+                <Modal.Body style={{marginLeft:"30px"}} >
                     <StarRating complex={complex} onClick={setShowModal(false)}/>
                 </Modal.Body>
                 
